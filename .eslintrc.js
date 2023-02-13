c976ad7c6e359c47ba234fa312b01e713c43670f
Martin Øvsttun <marovsttun@gmail.com>
2023-02-13 09:27:01 +0100

    part4a blog list exercise 4.1

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+	"env": {
+		"node": true,
+		"browser": true,
+		"commonjs": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [],
+	"parserOptions": {
+		"ecmaVersion": "latest"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"never"
+		],
+		"eqeqeq": "error",
+		"no-trailing-spaces": "error",
+		"object-curly-spacing": [
+			"error",
+			"never"
+		],
+		"arrow-spacing": [
+			"error",
+			{"before": true, "after": true}
+		],
+		"no-console": 0
+	}
+}
